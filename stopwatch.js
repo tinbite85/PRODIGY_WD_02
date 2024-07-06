9570588777a54a3d37bf4d215d29d6e64cc832d8
Tinbite Esayas <tinbiteesayas95@gmail.com>
2024-07-06 12:19:46 +0300

    configure : clear button, lap increment, minute increment

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,7 @@
 const startbtn = document.getElementById('start-btn');
 const lapbtn = document.getElementById('lap-btn');
 const resetbtn = document.getElementById('reset-btn');
+const clearbtn = document.getElementById('clear-btn');
 const SecondBucket = document.querySelector('.sec');
 const MinuteBucket = document.querySelector('.min');
 const milliSecBucket = document.querySelector('.milsec');
@@ -8,9 +9,10 @@ const lapItem = document.querySelector('.lap-item');
 
 
 
+
 let isStart = false;
 let ResetStatus = false;
-let secCount = 0;
+let secCount = 1;
 let minCount = 0;
 let milliSecCount = 0;
 let secondInterval;
@@ -35,35 +37,37 @@ const changebtn = () => {
 const start = () => {
     if (!isStart && !ResetStatus) {
         console.log('is true');
-        startbtn.innerHTML = 'pause';
+        startbtn.innerHTML = 'Pause';
         isStart = true;
 
         secondInterval = setInterval(() => {
             if (secCount === 60) {
                 secCount = 0;
+                minCount++;
+                MinuteBucket.innerHTML = `${minCount}:`;
             }
-            SecondBucket.innerHTML = secCount++;
+            SecondBucket.innerHTML = `${secCount++}:`;
         }, 1000);
 
-        minuteInterval = setInterval(() => {
-            if (minCount === 60) {
-                minCount = 0;
-            }
-            MinuteBucket.innerHTML = minCount++;
-        }, 60 * 1000);
+        // minuteInterval = setInterval(() => {
+        //     if (minCount === 60) {
+        //         minCount = 0;
+        //     }
+        //     MinuteBucket.innerHTML = minCount++;
+        // }, 60 * 1000);
 
         microsecInterval = setInterval(() => {
             if (milliSecCount === 100) {
                 milliSecCount = 0;
             }
-            milliSecBucket.innerHTML = milliSecCount++;
+            milliSecBucket.innerHTML = `${milliSecCount++}`;
         }, 10);
 
     }
 
     else {
         console.log('is false');
-        startbtn.innerHTML = 'start';
+        startbtn.innerHTML = 'Start';
         clearInterval(secondInterval);
         clearInterval(microsecInterval);
         clearInterval(minuteInterval);
@@ -78,7 +82,7 @@ const reset = () => {
     clearInterval(microsecInterval);
     clearInterval(minuteInterval);
     secCount = 0;
-    minCount = 0;  // Reset minCount as well
+    minCount = 0;  
     milliSecCount = 0;
     isStart = false;
     SecondBucket.innerHTML = "00 :";
@@ -90,14 +94,27 @@ const reset = () => {
     ResetStatus = false;
 
 }
+
+countInit= 0;
 const lap = () => {
+    countInit++;
     const lapTime = `${String(minCount).padStart(2, '0')}:${String(secCount).padStart(2, '0')}:${String(milliSecCount).padStart(2, '0')}`;
     const lapContent = document.createElement('li');
-    lapContent.textContent = lapTime;
+    lapContent.textContent = `${countInit}) ${lapTime}`;
     lapItem.appendChild(lapContent);
+   
 }
+
+const clearLap = () => {
+    lapItem.innerHTML = '';
+
+}
+
+
 
 
 startbtn.addEventListener("click", start);
 resetbtn.addEventListener("click", reset);
 lapbtn.addEventListener("click", lap);  
+clearbtn.addEventListener("click", clearLap);
+
